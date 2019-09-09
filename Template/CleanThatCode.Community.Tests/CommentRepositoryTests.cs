b445ebd481ef88864e8e9c7b31a0631688251f8b
Fridrik <tryggingamidstodin@tmhf.is>
2019-09-09 19:33:52 +0000

    Fyrstu test

--- Template/CleanThatCode.Community.Tests/CommentRepositoryTests.cs
+++ Template/CleanThatCode.Community.Tests/CommentRepositoryTests.cs
@@ -9,13 +9,15 @@ namespace CleanThatCode.Community.Tests {
         [TestMethod]
         public void GetAllCommentsByPostId_GivenWrongPostId_ShouldReturnNoComments() {
             //Ef að er gefið invalid post ID, asserta result á lengd 0
-            
-
+            var result = GetAllCommentsByPostId(100).length();
+            Assert.AreEqual(result, 0);
         }
 
         [TestMethod]
         public void GetAllCommentsByPostId_GivenValidPostId_ShouldReturnTwoComments() {
             //Ef að gefið er valid post ID, asserta result á lengd 2
+            var result = GetAllCommentsByPostId(1).length();
+            Assert.AreEqual(result, 2);
         }
 
     }
